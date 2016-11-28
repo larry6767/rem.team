75cc1c364e2e3c19718909b16155937874b8c961
larry6767 <shoegaze.larry@gmail.com>
2016-11-28 12:53:20 +0300

    add about, promo

--- src/js/main.js
+++ src/js/main.js
@@ -27,32 +27,32 @@ $(document).ready(function() {
       }]
   });
 
-//yandex map
-  YMaps.jQuery(function () {
-    var map = new YMaps.Map(YMaps.jQuery("#YMapsID")[0]);
-    map.setCenter(new YMaps.GeoPoint(39.702118,47.232759), 15);
+// //yandex map
+//   YMaps.jQuery(function () {
+//     var map = new YMaps.Map(YMaps.jQuery("#YMapsID")[0]);
+//     map.setCenter(new YMaps.GeoPoint(39.702118,47.232759), 15);
 
-    // create style
-    var s = new YMaps.Style();
+//     // create style
+//     var s = new YMaps.Style();
 
-    // create map-marker style
-    s.iconStyle = new YMaps.IconStyle();
-    s.iconStyle.href = "http://mers.roob.in/img/marker.png";
-    s.iconStyle.size = new YMaps.Point(48, 64);
-    s.iconStyle.offset = new YMaps.Point(-20, -65);
+//     // create map-marker style
+//     s.iconStyle = new YMaps.IconStyle();
+//     s.iconStyle.href = "http://mers.roob.in/img/marker.png";
+//     s.iconStyle.size = new YMaps.Point(48, 64);
+//     s.iconStyle.offset = new YMaps.Point(-20, -65);
 
-    var placemark = new YMaps.Placemark(new YMaps.GeoPoint(39.705918,47.227089), {style: s});
+//     var placemark = new YMaps.Placemark(new YMaps.GeoPoint(39.705918,47.227089), {style: s});
 
-    placemark.name = "Intacto";
-    placemark.description = "г. Ростов-на-Дону, ул. Греческого Города Волос, д. 6, оф. 513";
+//     placemark.name = "Intacto";
+//     placemark.description = "г. Ростов-на-Дону, ул. Греческого Города Волос, д. 6, оф. 513";
 
-    map.addOverlay(placemark);
+//     map.addOverlay(placemark);
 
-  });
+//   });
 
-  $(".map-mask").click(function (e) {
-    $(".map-mask").remove();
-  });
+//   $(".map-mask").click(function (e) {
+//     $(".map-mask").remove();
+//   });
 
 
 //scroll
